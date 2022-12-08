6c95b0c5ef901140c50afa3f951415a6720edca2
Adam Roxann Irsyad <adam354oke@gmail.com>
2022-12-08 12:53:37 +0700

    feat: :art: Update Add and Update Employee

--- lib/provider/employee_provider.dart
+++ lib/provider/employee_provider.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:sdm_priok/models/responses/response_detail_employee.dart';
 import 'package:sdm_priok/models/responses/response_employee.dart';
+import 'package:async/async.dart';
+import 'package:path/path.dart' as path;
 
 import '../helpers/data_helper.dart';
 
@@ -43,6 +46,97 @@ class EmployeeProvider extends ChangeNotifier {
   //
   //   }
   // }
+
+  Future<bool> addEmployee(String nip, String name, String phone, String email, String password, String team_id, String role_id, File image) async {
+    try {
+      var stream =
+          http.ByteStream(DelegatingStream.typed(image.openRead()));
+      var length = await image.length();
+      var uri = Uri.parse("https://humancapitalpriokpomu.com/api/employee/store");
+      final request = http.MultipartRequest("POST", uri);
+      request.fields['nip'] = nip;
+      request.fields['name'] = name;
+      request.fields['phone'] = phone;
+      request.fields['email'] = email;
+      request.fields['password'] = password;
+      request.fields['team_id'] = team_id;
+      request.fields['role_id'] = role_id;
+      request.files.add(http.MultipartFile("image", stream, length,
+          filename: path.basename(image.path)));
+      var response = await request.send();
+      if (response.statusCode == 200) {
+      notifyListeners();
+      return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      debugPrint("Error $e");
+      return false;
+    }
+  }
+
+  Future<bool> updateEmployeeWithoutImage(String hash, String nip, String name, String phone, String email, String team_id, String role_id) async {
+    try {
+      // var stream =
+          // http.ByteStream(DelegatingStream.typed(image.openRead()));
+      // var length = await image.length();
+      var uri = Uri.parse("https://humancapitalpriokpomu.com/api/employee/update/"+hash);
+      final request = http.MultipartRequest("POST", uri);
+      request.fields['nip'] = nip;
+      request.fields['name'] = name;
+      request.fields['phone'] = phone;
+      request.fields['email'] = email;
+      request.fields['team_id'] = team_id;
+      request.fields['role_id'] = role_id;
+      // request.files.add(http.MultipartFile("image", stream, length,
+          // filename: path.basename(image.path)));
+      var response = await request.send();
+      // final respStr = await response.stream.bytesToString();
+      if (response.statusCode == 200) {
+      // print(respStr);
+      notifyListeners();
+      return true;
+      } else {
+        // print(respStr);
+        return false;
+      }
+    } catch (e) {
+      debugPrint("Error $e");
+      return false;
+    }
+  }
+
+  Future<bool> updateEmployee(String hash, String nip, String name, String phone, String email, String team_id, String role_id, File image) async {
+    try {
+      var stream =
+          http.ByteStream(DelegatingStream.typed(image.openRead()));
+      var length = await image.length();
+      var uri = Uri.parse("https://humancapitalpriokpomu.com/api/employee/update/"+hash);
+      final request = http.MultipartRequest("POST", uri);
+      request.fields['nip'] = nip;
+      request.fields['name'] = name;
+      request.fields['phone'] = phone;
+      request.fields['email'] = email;
+      request.fields['team_id'] = team_id;
+      request.fields['role_id'] = role_id;
+      request.files.add(http.MultipartFile("image", stream, length,
+          filename: path.basename(image.path)));
+      var response = await request.send();
+      // final respStr = await response.stream.bytesToString();
+      if (response.statusCode == 200) {
+      notifyListeners();
+      // print(respStr);
+      return true;
+      } else {
+        // print(respStr);
+        return false;
+      }
+    } catch (e) {
+      debugPrint("Error $e");
+      return false;
+    }
+  }
 
   Future<bool> deleteEmployee(String hash) async {
     final url = 'https://humancapitalpriokpomu.com/api/employee/delete/' + hash;
